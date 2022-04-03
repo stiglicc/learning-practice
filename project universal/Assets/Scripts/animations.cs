b447e8b83bac282328d2116b0581a56adfb7b963
stiglicc <nikolastiglic000@gmail.com>
2022-04-03 21:17:11 +0200

    added-animations
    Player is now animated moving forward, backward and sideways

---
+++ project universal/Assets/Scripts/animations.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animations : MonoBehaviour
+{
+    [SerializeField] Animator anima;
+    
+    void Start()
+    {
+        anima = GetComponent<Animator>();  
+    }
+
+    
+    void Update()
+    {
+        walkForward();
+        walkBack();
+        walkRight();
+        walkLeft();
+    }
+    private void walkForward() {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            anima.SetBool("isWalkingForward", true);
+        }
+        else anima.SetBool("isWalkingForward", false);
+    }
+    private void walkBack() {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            anima.SetBool("isWalkingBack", true);
+        }
+        else anima.SetBool("isWalkingBack", false);
+    }
+    private void walkRight() {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            anima.SetBool("isWalkingRight", true);
+        } else anima.SetBool("isWalkingRight", false);
+    }
+    private void walkLeft() {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            anima.SetBool("isWalkingLeft", true);
+        } else anima.SetBool("isWalkingLeft", false);
+    }
+    
+}
